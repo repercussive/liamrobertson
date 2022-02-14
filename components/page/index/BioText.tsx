d7f8ad3721a83a2e2d43e0c81409a321999bb6fc
= <liam2robertson@gmail.com>
2022-02-14 19:46:58 +0000

    📱 small responsiveness improvements

--- components/page/index/BioText.tsx
+++ components/page/index/BioText.tsx
@@ -1,14 +1,23 @@
+import { styled } from '@/styles/stitches.config'
 import Text from '@/components/modular/Text'
 
 const BioText = () => {
   return (
     <Text>
       I write code and make music.{' '}
-      I fancy myself somewhat of a <strong>front-end developer</strong>.<br />
+      I fancy myself somewhat of a <strong>front-end developer</strong>.{' '}
+      <LineBreak />
       For about 10 years I've lived in <strong>Glasgow</strong>.{' '}
       You've stumbled upon a portfolio page of sorts.
     </Text>
   )
 }
+
+const LineBreak = styled('br', {
+  display: 'none',
+  '@media screen and (min-width: 780px)': {
+    display: 'block'
+  }
+})
 
 export default BioText
--- components/page/index/HeadingSection.tsx
+++ components/page/index/HeadingSection.tsx
@@ -1,26 +1,30 @@
 import Flex from 'components/modular/Flex'
 import Text from '@/components/modular/Text'
 import Spacer from '@/components/modular/Spacer'
+import { styled } from '@/styles/stitches.config'
 
 const HeadingSection = () => {
   return (
     <Flex align="center">
-      <Text
-        role="presentation"
-        as="span"
-        css={{ fontSize: '2rem', '@bp1': { fontSize: '4rem' } }}
-      >
-        👋
-      </Text>
+      <WaveEmoji role="presentation">👋</WaveEmoji>
       <Spacer mr="$3" />
-      <Text
-        as="h1"
-        css={{ fontSize: '1.75rem', '@bp1': { fontSize: '2.5rem' } }}
-      >
-        Hi, my name is <br /><strong>Liam Robertson.</strong>
-      </Text>
-    </Flex>
+      <MainHeading>Hi, my name is <br /><strong>Liam Robertson.</strong></MainHeading>
+    </Flex >
   )
 }
+
+const WaveEmoji = styled('span', {
+  fontSize: '2rem',
+  '@bp1': { fontSize: '4rem' }
+})
+
+const MainHeading = styled('h1', {
+  position: 'relative',
+  top: '1px',
+  fontSize: '1.75rem',
+  fontWeight: 'normal',
+  '@bp1': { fontSize: '2.25rem' },
+  '@media screen and (max-width: 325px)': { fontSize: '1.25rem' }
+})
 
 export default HeadingSection
